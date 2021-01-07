43b9401f5a02e47500048767f609e10af9c7ad1c
(no1gun2) <(no1gun2@naver.com)>
2021-01-07 10:48:08 +0900

    scan

--- PortScan/tcp_scan.py
+++ PortScan/tcp_scan.py
@@ -1,13 +1,11 @@
 # tcp scan
 import socket
-
+# cmd -> netstat -ano
+# 열려있는 포트 확인 가능
 with socket.socket() as s:
-    addr = ("127.0.0.1",8080)
+    addr = ("127.0.0.1", 8080)
     try:
         s.connect(addr)
         print("80 tcp socket is opened")
     except:
         print("80 tcp socket is closed")
-        
-    
-    
